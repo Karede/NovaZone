074f26703497179ca85ebfe20d029ff4d0c06c0a
Matthew <parityhero@users.noreply.github.com>
2018-02-12 15:16:48 -0400

    footer fix

--- footer.php
+++ footer.php
@@ -1,20 +1,16 @@
-<footer id="footer" role="contentinfo">
-        <div class="row mt-4">
-            <div class="col-sm-12 no-padding">
-                <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/images/footer-top.png" alt="<?php bloginfo('name'); ?>">
-            </div><!--/.col-sm-12-->
-        </div><!--/.row-->
-        <div class="row contact justify-content-center">
-            <div class="col-12 col-md-6">
-                <h2 class="contact-title mt-4">CONTACT US</h2>
-                <?php echo do_shortcode('[contact-form-7 id="6" title="NovaZone Contact Form"]'); ?>
-            </div><!--/.col-12-->
-        </div><!--/.row-->
-        <div class="row">
-            <div class="col-sm-12 no-padding">
-                <img class="w-100 justify-content-center" src="<?php echo get_template_directory_uri(); ?>/images/footer-bottom.png" alt="<?php bloginfo('name'); ?>">
-            </div><!--/.col-sm-12-->
-        </div><!--/.row-->
+<footer class="mt-5" id="footer" role="contentinfo">
+    <img class="footer-top w-100" src="<?php echo get_template_directory_uri(); ?>/images/footer-top.png" alt="<?php bloginfo('name'); ?>">
+    <div class="background">
+        <div class="container pt-5">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6">
+                    <h2 class="mt-4">CONTACT US</h2>
+                    <?php echo do_shortcode('[contact-form-7 id="6" title="NovaZone Contact Form"]'); ?>
+                </div><!--/.col-12-->
+            </div><!--/.row-->
+        </div><!--/.container-->
+        <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/images/footer-bottom.png" alt="<?php bloginfo('name'); ?>">
+    </div><!--/.background-->
 </footer><!--/.footer-->
 
 <?php wp_footer(); ?>
